2166d9412abf3ea6f34719bf2a4ca50718b5acd9
romeech91 <romontino@gmail.com>
2019-03-14 20:18:05 +0300

    modal window add to features sec

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 //= js/slider.js
 //= js/popups.js
+//= js/features-modal.js
---
+++ src/js/features-modal.js
@@ -0,0 +1,40 @@
+const items = document.querySelectorAll('.features__item');
+const featuresList = document.querySelector('.features__list');
+
+items.forEach(element => {
+    element.addEventListener('click', function(event) {
+        let current = event.target;
+        let currentImage = current.firstElementChild.firstElementChild;
+        let currentDescription = current.lastElementChild.textContent;
+        let currentSRC = currentImage.currentSrc;
+        let newWindow = document.createElement('div');
+        let closeBtn = document.createElement('div');
+        let articleHeading = document.createElement('h4');
+        articleHeading.classList.add('article-heading');
+        articleHeading.textContent = currentDescription;
+        featuresList.appendChild(newWindow);
+        newWindow.classList.add('features-article');
+        closeBtn.classList.add('close-btn');
+        newWindow.appendChild(closeBtn);
+        let imageContent = document.createElement('div');
+        imageContent.classList.add('image-content');
+        imageContent.style.backgroundImage = "url(" + currentSRC + ")";
+        imageContent.style.backgroundSize = 'cover';
+        newWindow.appendChild(imageContent);
+        newWindow.appendChild(articleHeading);
+        let articleDesc = document.createElement('p');
+        articleDesc.classList.add('article-text');
+        articleDesc.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum.';
+        newWindow.appendChild(articleDesc);
+        closeBtn.addEventListener('click', function () {
+            featuresList.removeChild(newWindow);
+        })
+    })
+});
+
+// items.forEach(el => {
+//     el.addEventListener('click', function(e) {
+//         let curr = e.target;
+//         console.log(curr.firstElementChild.firstElementChild);
+//     })
+// });
