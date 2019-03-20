1679abae0416d64f723b1ab10c9af87056eb450b
romeech91 <romontino@gmail.com>
2019-03-20 13:19:35 +0300

    add to cart feature complete

--- src/js/buynow.js
+++ src/js/buynow.js
@@ -1,22 +1,41 @@
 const totalPrice = document.querySelector('.total-price');
 const buttons = document.querySelectorAll('.buy-button');
 const cart = document.querySelector('.shopping-bag');
-const orderForm = document.querySelector('.order-form');
-
+const orderForm = document.querySelector('.form');
+const formTotal = document.querySelector('.form__total-result');
+const productName = document.querySelector('.form__product-result');
+const cancel = document.querySelector('.form__cancel');
+let productNameCount = '';
 let cartCount = 0;
+let totalItemsInCart = 0;
 
 buttons.forEach(btn => {
 	btn.addEventListener('click', (e) => {
 		let currentBtn = e.target;
+		let currentProductName = currentBtn.previousElementSibling;
 		let currentPrice = currentBtn.getAttribute('data-price');
 		let numberValue = parseInt(currentPrice, 10);
 
 		cartCount += numberValue;
 		totalPrice.textContent = cartCount + '$';
+		formTotal.textContent = cartCount + '$';
+		totalItemsInCart++;
+		productNameCount += currentProductName.textContent + ', ';
+		productName.textContent = productNameCount;
+
 	})
 });
 
 cart.addEventListener('click', () => {
-	orderForm.style.display = 'block';
+	orderForm.style.left = '50%';
 });
 
+cancel.addEventListener('click', () => {
+	orderForm.style.left = '-9000px';
+	productNameCount = '';
+	cartCount = 0;
+	totalItemsInCart = 0;
+	formTotal.textContent = '';
+	productName.textContent = '';
+	totalPrice.textContent = '0$';
+});
