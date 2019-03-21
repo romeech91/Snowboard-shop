9770e5c93ee5e32ad5eafdd5717d89658e2a7800
romeech91 <romontino@gmail.com>
2019-03-22 02:55:09 +0300

    features modal full complete

--- src/js/features-modal.js
+++ src/js/features-modal.js
@@ -1,38 +1,51 @@
-const items = document.querySelectorAll('.features__item');  //li
-const featuresList = document.querySelector('.features__list');  //ul
+const featuresButtons = document.querySelectorAll('.features__item');
+const featuresList = document.querySelector('.features__list');
 
-items.forEach(element => {
+featuresButtons.forEach(btn => {
+	btn.addEventListener('click', evt => {
+		let current = event.target;
+		let createModal = function () {
+			let currentImage = current.firstElementChild.firstElementChild;
+			let currentDescription = current.lastElementChild.textContent;
+			let currentSRC = currentImage.currentSrc;
+			let newWindow = document.createElement('div');
+			let closeBtn = document.createElement('div');
+			let articleHeading = document.createElement('h4');
 
-    element.addEventListener('click', function(event) {
-        let current = event.target;
+			articleHeading.classList.add('article-heading');
+			articleHeading.textContent = currentDescription;
+			featuresList.appendChild(newWindow);
+			newWindow.classList.add('features-article');
+			closeBtn.classList.add('close-btn');
+			newWindow.appendChild(closeBtn);
+			let imageContent = document.createElement('div');
+			imageContent.classList.add('image-content');
+			imageContent.style.backgroundImage = "url(" + currentSRC + ")";
+			imageContent.style.backgroundSize = 'cover';
+			newWindow.appendChild(imageContent);
+			newWindow.appendChild(articleHeading);
+			let articleDesc = document.createElement('p');
+			articleDesc.classList.add('article-text');
+			articleDesc.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit.';
+			newWindow.appendChild(articleDesc);
 
-        let currentImage = current.firstElementChild.firstElementChild;
-        let currentDescription = current.lastElementChild.textContent;
-        let currentSRC = currentImage.currentSrc;
-        let newWindow = document.createElement('div');
-        let closeBtn = document.createElement('div');
-        let articleHeading = document.createElement('h4');
-
-        articleHeading.classList.add('article-heading');
-        articleHeading.textContent = currentDescription;
-        featuresList.appendChild(newWindow);
-        newWindow.classList.add('features-article');
-        closeBtn.classList.add('close-btn');
-        newWindow.appendChild(closeBtn);
-        let imageContent = document.createElement('div');
-        imageContent.classList.add('image-content');
-        imageContent.style.backgroundImage = "url(" + currentSRC + ")";
-        imageContent.style.backgroundSize = 'cover';
-        newWindow.appendChild(imageContent);
-        newWindow.appendChild(articleHeading);
-        let articleDesc = document.createElement('p');
-        articleDesc.classList.add('article-text');
-        articleDesc.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolore ducimus eius eum, itaque laboreminima nemo nulla odio, odit pariatur placeat quidem recusandae saepe sed suscipit veritatis, voluptatibusvoluptatum.';
-        newWindow.appendChild(articleDesc);
-
-        closeBtn.addEventListener('click', function () {
-            featuresList.removeChild(newWindow);
-        })
-    })
+			closeBtn.addEventListener('click', function () {
+				featuresList.removeChild(newWindow);
+			})
+		};
+		if (current !== btn) {
+			evt.stopPropagation();
+			if (current !== btn) {
+				current = current.parentElement;
+				if (current === btn) {
+					createModal();
+				} else {
+					current = current.parentElement;
+					createModal();
+				}
+			}
+		} else if (current === btn) {
+			createModal();
+		}
+	})
 });
-
